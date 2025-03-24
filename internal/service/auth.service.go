b7e2af6d258c3490fbbff1bc8c5ba839e0c30a04
muruga21 <murugaperumal21062004@gmail.com>
2025-03-24 12:10:56 +0530

    added redis, and exposed port in docker image

--- internal/service/auth.service.go
+++ internal/service/auth.service.go
@@ -2,9 +2,19 @@ package service
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+
+	"github.com/redis/go-redis/v9"
 )
 
 func HandleSignup(w http.ResponseWriter, r *http.Request) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // No password set
+		DB:       0,  // Use default DB
+		Protocol: 2,  // Connection protocol
+	})
+	fmt.Println(client)
 	json.NewEncoder(w).Encode(map[string]string{"error": "false", "message": "hello world"})
 }
