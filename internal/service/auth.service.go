f7fa84501276c23af202008bde1d4e2cfda8b3de
muruga21 <murugaperumal21062004@gmail.com>
2025-03-24 13:30:07 +0530

    testing with redis

--- internal/service/auth.service.go
+++ internal/service/auth.service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -15,6 +16,15 @@ func HandleSignup(w http.ResponseWriter, r *http.Request) {
 		DB:       0,  // Use default DB
 		Protocol: 2,  // Connection protocol
 	})
+	ctx := context.Background()
+
+	err := client.Set(ctx, "foo", "bar", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := client.Get(ctx, "foo").Result()
+
 	fmt.Println(client)
-	json.NewEncoder(w).Encode(map[string]string{"error": "false", "message": "hello world"})
+	json.NewEncoder(w).Encode(map[string]string{"error": "false", "message": val})
 }
